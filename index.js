360caec1c1abb3906636b74229c62337332f77a2
Ethel Cofresi <cofresi.ethel@gmail.com>
2019-02-08 20:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,64 +1,43 @@
-// import { create } from "domain";
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
 
-$(document).ready(function(){
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
 
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
+$(document).ready(function (){
 });
-
- function displayError() {
-     $('#errors').html("Im sorry, there's been an error.Please try again.");
- }
-function searchRepositories(){
-    let data = $("#searchTerms").val();
-   let url = `https://api.github.com/search/repositories?q=${data}/`
-
-    $.get(url,function(response){
-        var result = `<ul>${response.items.map(r => '<li>' + r.name +
-                        `</li><a href="#" data-owner="${r.owner.login}",
-                            data-result="${r.name}" onclick="showCommits(this)">
-                               Show Commits</a>`)}</ul>`
-       
-        $("#results").html(result)
-
-    }    
-   ) .fail(function(error){
-       console.log(displayError)
-   })
-  }
-
-
-//  in  showCommits, function that gets the repository's 
-// commits from the GitHub API and display them in the details div.
-//list the SHA, the author, the author's login, and the author's avatar as an image.
-
-  function showCommits(element) {
-      console.log(element)
-    //   debugger
-
-    document.getElementById('#repositories')
-        let commitURL = `https://api.github.com/repos/${element.dataset.owner}/${element.dataset.result}/commits`
-       
-        $.get(commitURL,data => {
-
-    //For Testing        
-    //     let data_out = data.map(commit => commit.author.login)
-    //     console.log(data_out)
-
-    //List the SHA, the author, the author's login, and the author's avatar as an image.  
-         var commitsList =  `${data.map(commit =>  commit.author.login + `<br>` +
-                                                    commit.sha + `<br>` +
-                                                    `<img src=` + commit.author.avatar_url + `>`
-         
-         )}`
-
-        $("#details").html(commitsList)
-  
-        }
-           ).fail(function(error){
-             console.log(displayError)
-        });
-    }
-
-
-
-
-
